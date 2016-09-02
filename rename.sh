86546e39b06a5ed1077a8d2b0936ba3412bcd3f4
374632897 <374632897@qq.com>
2016-09-02 09:40:43 +0800

    add rename file

--- rename.sh
+++ rename.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 mv Gruntfile-2.js Gruntfile-tmp.js
-mv Grunfile.js Gruntfile-2.js
-mv Gruntfile-tmp.js Gruntfile.js
+
+mv ./Gruntfile.js ./Gruntfile-2.js
+
+mv ./Gruntfile-tmp.js ./Gruntfile.js
